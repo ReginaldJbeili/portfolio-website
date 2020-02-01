2bc35ccc0ff3e4c625ad905bb9c67da4bf652383
ReginaldJbeili <reginaldjbeili@gmail.com>
2020-02-01 17:52:22 +0000

    create bare about me page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,13 +1,75 @@
 import React from "react";
 
 const About = () => {
+  const [toggleMe, setToggleMe] = React.useState(true);
+  const [toggleDev, setToggleDev] = React.useState(false);
+  const [toggleNurse, setToggleNurse] = React.useState(false);
+  const [toggleGamer, setToggleGamer] = React.useState(false);
+  const [toggleFriend, setToggleFriend] = React.useState(false);
+
   return (
     <section>
-      <h2 id="about">My story</h2>
-      <h3>I'm a developer</h3>
-      <h3>I'm a nurse</h3>
-      <h3>I'm a gamer</h3>
-      <h3>I'm a friend</h3>
+      <h2>Me</h2>
+      <button
+        onClick={() => {
+          setToggleMe(true);
+          setToggleDev(false);
+          setToggleNurse(false);
+          setToggleGamer(false);
+          setToggleFriend(false);
+        }}
+      >
+        This is me
+      </button>
+      <button
+        onClick={() => {
+          setToggleMe(false);
+          setToggleDev(true);
+          setToggleNurse(false);
+          setToggleGamer(false);
+          setToggleFriend(false);
+        }}
+      >
+        I'm a developer
+      </button>
+      <button
+        onClick={() => {
+          setToggleMe(false);
+          setToggleDev(false);
+          setToggleNurse(true);
+          setToggleGamer(false);
+          setToggleFriend(false);
+        }}
+      >
+        I'm a nurse
+      </button>
+      <button
+        onClick={() => {
+          setToggleMe(false);
+          setToggleDev(false);
+          setToggleNurse(false);
+          setToggleGamer(true);
+          setToggleFriend(false);
+        }}
+      >
+        I'm a gamer
+      </button>
+      <button
+        onClick={() => {
+          setToggleMe(false);
+          setToggleDev(false);
+          setToggleNurse(false);
+          setToggleGamer(false);
+          setToggleFriend(true);
+        }}
+      >
+        I'm a friend
+      </button>
+      {toggleMe ? "My story" : null}
+      {toggleDev ? "My Developer Story" : null}
+      {toggleNurse ? "My Nurse Story" : null}
+      {toggleGamer ? "My Gamer Story" : null}
+      {toggleFriend ? "My Friend Story" : null}
     </section>
   );
 };
--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ReactPlayer from "react-player";
 
 const Landing = () => {
   return (
     <section>
       <h1 id="#home">Reginald Jbeili</h1>
+      <ReactPlayer url="https://i.imgur.com/P6IDx1r.mp4" playing />
     </section>
   );
 };
