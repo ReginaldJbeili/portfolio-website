6c4982d0be1569b990fbc1952bdb97ffeda91894
ReginaldJbeili <reginaldjbeili@gmail.com>
2020-06-05 16:31:50 +0100

    modify background and landing page content

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -5,9 +5,15 @@ import H1 from "../Styles/H1.style";
 const Landing = () => {
   return (
     <section id="home" className="landing-page">
+      <H1>I am</H1>
+
       <H1>Reginald Jbeili</H1>
 
-      <H1>Full-Stack Web Developer</H1>
+      <H1 className="h1-sub">Software Developer</H1>
+
+      <H1>Welcome to my website</H1>
+
+      <H1 className="h1-sub">Go ahead and grab a CV</H1>
     </section>
   );
 };
